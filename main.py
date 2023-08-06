fd7fb33e7628d4f4f1d30d75e2598daee407762f
kishanrajeev <kishanrajeev>
2023-08-06 14:43:38 -0400

    Changed imports/file to tf instead of h5

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import os
 import tensorflow as tf
 from keras.datasets import cifar10
-from tensorflow import keras
 from tensorflow.python.keras import layers
+import keras.utils
 
-class CustomModuleWrapper(tf.Module):
+class ModuleWrapper(keras.layers.Layer):
     def __init__(self, module, **kwargs):
         super().__init__(**kwargs)
         self.module = module
 
-    def __call__(self, *args, **kwargs):
-        return self.module(*args, **kwargs)
+    def call(self, inputs, training=False):
+        return self.module(inputs, training=training)
 
     def get_config(self):
-        return {"module": keras.utils.serialize_keras_object(self.module)}
+        config = super().get_config()
+        config.update({
+            "module": keras.utils.serialize_keras_object(self.module),
+        })
+        return config
+
 
 # Load and normalize dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
@@ -24,8 +29,8 @@ x_test = x_test.astype("float32") / 255.0
 model = keras.Sequential([
     layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
-    CustomModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
-    CustomModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
+    ModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
+    ModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
     layers.Flatten(),
     layers.Dense(64, activation="relu"),
     layers.Dense(10),
@@ -45,14 +50,7 @@ model.fit(x_train, y_train, batch_size=4, epochs=10)
 model.evaluate(x_test, y_test)
 
 # Save the model to the project files next to the .py file
-save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'my_model.h5')
-
-# Implement get_config() for each layer in the model
-for layer in model.layers:
-    if isinstance(layer, CustomModuleWrapper):
-        layer_config = layer.get_config()
-        module = keras.utils.deserialize_keras_object(layer_config["module"])
-        layer.module = module
+save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'my_model.tf')
 
 # Save the model
-model.save(save_path, save_format='h5')
+model.save(save_path, save_format='tf')
