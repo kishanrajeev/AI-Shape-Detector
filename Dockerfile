535c597fc8b68299105999aeee30a70aa303a5c8
kishanrajeev <kishanrajeev>
2023-08-05 23:08:36 -0400

    Running from Docker in Arch with ROCm

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM tensorflow/tensorflow:latest
+
+RUN pip install keras
+RUN pip install datasets
--- main.py
+++ main.py
@@ -1,72 +1,34 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.python.keras import layers
+from tensorflow.python.keras.datasets import cifar10
+
 
 # Load and normalize dataset
-transform = transforms.Compose(
-    [transforms.ToTensor(),
-     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-
-trainset = datasets.CIFAR10(root='./data', train=True,
-                                        download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=4,
-                                          shuffle=True, num_workers=2)
-
-testset = datasets.CIFAR10(root='./data', train=False,
-                                       download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=4,
-                                         shuffle=False, num_workers=2)
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
 
 # Define the model
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 32, 3)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.conv3 = nn.Conv2d(64, 64, 3)
-        self.fc1 = nn.Linear(64 * 4 * 4, 64)
-        self.fc2 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 64 * 4 * 4)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-net = Net()
+model = keras.Sequential([
+    layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.Conv2D(64, (3, 3), activation="relu"),
+    layers.Flatten(),
+    layers.Dense(64, activation="relu"),
+    layers.Dense(10),
+])
 
 # Define a Loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters())
+loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+optimizer = keras.optimizers.Adam()
+
+# Compile the model
+model.compile(optimizer=optimizer, loss=loss_fn, metrics=["accuracy"])
 
 # Train the network
-for epoch in range(10):  # loop over the dataset multiple times
+model.fit(x_train, y_train, batch_size=4, epochs=10)
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-
-print('Finished Training')
+# Evaluate the model
+model.evaluate(x_test, y_test)
