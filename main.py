26cc587f509a0474d72a7d86e4f5e9f1269a7887
kishanrajeev <kishanrajeev>
2023-08-06 13:36:19 -0400

    Updated code for the save_path fucntion.

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ from keras.datasets import cifar10
 from tensorflow import keras
 from tensorflow.python.keras import layers
 
+class CustomModuleWrapper(tf.Module):
+    def __init__(self, module, **kwargs):
+        super().__init__(**kwargs)
+        self.module = module
+
+    def __call__(self, *args, **kwargs):
+        return self.module(*args, **kwargs)
+
+    def get_config(self):
+        return {"module": keras.utils.serialize_keras_object(self.module)}
+
 # Load and normalize dataset
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 x_train = x_train.astype("float32") / 255.0
@@ -13,8 +24,8 @@ x_test = x_test.astype("float32") / 255.0
 model = keras.Sequential([
     layers.Conv2D(32, (3, 3), activation="relu", input_shape=(32, 32, 3)),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation="relu"),
-    layers.Conv2D(64, (3, 3), activation="relu"),
+    CustomModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
+    CustomModuleWrapper(layers.Conv2D(64, (3, 3), activation="relu")),
     layers.Flatten(),
     layers.Dense(64, activation="relu"),
     layers.Dense(10),
@@ -33,6 +44,15 @@ model.fit(x_train, y_train, batch_size=4, epochs=10)
 # Evaluate the model
 model.evaluate(x_test, y_test)
 
-# Save the model to the documents folder
-save_path = os.path.expanduser("~/Documents/my_model.h5")
+# Save the model to the project files next to the .py file
+save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'my_model.h5')
+
+# Implement get_config() for each layer in the model
+for layer in model.layers:
+    if isinstance(layer, CustomModuleWrapper):
+        layer_config = layer.get_config()
+        module = keras.utils.deserialize_keras_object(layer_config["module"])
+        layer.module = module
+
+# Save the model
 model.save(save_path)
